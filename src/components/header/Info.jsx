1fb022b8401055ab82fa63c14e3279cb79149aeb
dreamxinxcode <dreamxinxcode@gmail.com>
2021-02-12 09:37:01 -0800

    set up api call with axios

--- src/components/header/Info.jsx
+++ src/components/header/Info.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Info.scss';
+import { SearchContext } from '../../SearchContext';
 
 function Info() {
+  const [search] = useContext(SearchContext);
+
   return (
     <div id='info'>
-      
+      <div id="ip">
+        <h3>IP ADDRESS</h3>
+        <span>{search.ip}</span> 
+      </div>
+
+      <div id="location">
+        <h3>LOCATION</h3>
+        <span>{search.city}</span>
+        <span>{search.regionName}</span>
+        <span>{search.country}</span>      
+      </div>
+
+      <div id="timezone">
+        <h3>TIMEZONE</h3>
+        <span></span>
+      </div>
+
+      <div id="isp">
+        <h3>ISP</h3>
+        <span>{search.isp}</span>
+      </div>
     </div>
   )
 }
--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -2,21 +2,29 @@ import React, { useContext } from 'react'
 import { SearchContext } from '../../SearchContext';
 import './Search.scss';
 import  { searchContext } from '../../SearchContext';
+import axios from 'axios';
 
 function Search() {
   const [search, setSearch] = useContext(SearchContext);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleChange = (event) => {
     const query = event.target.value;
     setSearch(prev => ({...prev, query }));
-    console.log(search);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.get(`http://ip-api.com/json/${search.query}`)
+    .then(res => setSearch(prev => ({...prev, ip: res.data.query, city: res.data.city, country: res.data.country, isp: res.data.isp, lat: res.data.lat, lon: res.data.lon, regionName: res.data.regionName})))
+    .catch(err => console.log(err));
   }
 
   return (
     <div id='search'>
-      <input onChange={handleSubmit} type="text" placeholder='Search for any IP address or domain' />
-      <button type="submit"></button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" placeholder='Search for any IP address or domain' />
+        <button type="submit">></button>
+      </form>
     </div>
   )
 }
